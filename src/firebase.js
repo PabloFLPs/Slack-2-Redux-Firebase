27fe2087ebbb83c42fa82c27761df02942d952c2
Pablo Felipe <pablofelipe1999@hotmail.com>
2022-04-11 04:05:01 -0300

    Setting up the firebase.js config file

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCb8tFyUEJfNniOxdgevJloGlE7P2MTRes",
+    authDomain: "slack-clone-redux-firebase.firebaseapp.com",
+    projectId: "slack-clone-redux-firebase",
+    storageBucket: "slack-clone-redux-firebase.appspot.com",
+    messagingSenderId: "565316896433",
+    appId: "1:565316896433:web:f2771346d5e2f43ab54490",
+    measurementId: "G-KCS76LYW9V"
+}
