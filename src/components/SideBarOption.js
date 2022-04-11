8879b4288232f0bbf16c6d046beea422907d41b4
Pablo Felipe <pablofelipe1999@hotmail.com>
2022-04-11 06:55:58 -0300

    Creating SideBarOption component

---
+++ src/components/SideBarOption.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+// Styled-components:
+import styled from 'styled-components'
+
+function SideBarOption({ Icon, title, addChannelOption }) {
+    const addChannel = () => {}
+
+    const selectChannel = () => {}
+
+    return (
+        <SideBarOptionContainer onClick={addChannelOption ? addChannel : selectChannel}>
+            {Icon && <Icon fontsize="small" style={{ padding: 10 }} />}
+            {Icon ? (
+                <h3>{title}</h3>
+            ) : (
+                <SideBarOptionChannel>
+                    <span>#</span> {title}
+                </SideBarOptionChannel>
+            )}
+        </SideBarOptionContainer>
+    )
+}
+
+export default SideBarOption
+
+const SideBarOptionContainer = styled.div`
+    display: flex;
+    font-size: 12px;
+    align-items: center;
+    padding-left: 2px;
+    cursor: pointer;
+
+    :hover {
+        opacity: 0.9;
+        background-color: #340e36;
+    }
+
+    > h3 {
+        font-weight: 500;
+    }
+
+    > h3 > span {
+        padding: 15px;
+    }
+`
+
+const SideBarOptionChannel = styled.div`
+
+`
