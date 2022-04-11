156d8f2ea3317f52ace76266b3c3928d6b8a5a72
Pablo Felipe <pablofelipe1999@hotmail.com>
2022-04-11 06:58:43 -0300

    Importing firebase for config

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase"
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyCb8tFyUEJfNniOxdgevJloGlE7P2MTRes",
