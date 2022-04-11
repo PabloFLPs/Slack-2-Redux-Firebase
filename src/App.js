6deda1050d9f3f0eeded6244bf7944e93307daf4
Pablo Felipe <pablofelipe1999@hotmail.com>
2022-04-11 05:55:34 -0300

    Restructuring App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import './App.css'
 
+// React Router DOM:
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom"
+
+// Components:
+import Header from './components/Header';
+
 function App() {
   return (
     <div className="App">
-      <h1>Let's Build Slack</h1>
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<Header />}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
