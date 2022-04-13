01b7809ef680d06259be4482bf01a83b4ee1499f
Pablo Felipe <pablofelipe1999@hotmail.com>
2022-04-13 09:31:55 -0300

    Creating our rooms reducer

---
+++ src/features/appSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  roomId: null,
+}
+
+export const appSlice = createSlice({
+  name: 'app',
+  initialState,
+  // The `reducers` field lets us define reducers and generate associated actions
+  reducers: {
+    enterRoom: (state, action) => {
+      state.roomId = action.payload.roomId
+    }
+  }
+})
+
+export const { enterRoom } = appSlice.actions;
+
+export const selectRoomId = state => state.app.roomId
+
+export default appSlice.reducer
