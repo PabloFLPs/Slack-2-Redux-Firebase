4b3e15a3c8f4e162bd016da549a14cf292adff81
Pablo Felipe <pablofelipe1999@hotmail.com>
2022-04-13 07:32:58 -0300

    Fixing "module not found" error and wrong declaration of "cont firebase" to "const firebaseApp"

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
-import firebase from "firebase"
+import firebase from "firebase/compat/app"
+import "firebase/compat/auth"
+import "firebase/compat/firestore"
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -11,7 +13,7 @@ const firebaseConfig = {
     measurementId: "G-KCS76LYW9V"
 }
 
-const firebase = firebase.initializeApp(firebaseConfig)
+const firebaseApp = firebase.initializeApp(firebaseConfig)
 const db = firebaseApp.firestore()
 const auth = firebase.auth()
 const provider = new firebase.auth.GoogleAuthProvider()
