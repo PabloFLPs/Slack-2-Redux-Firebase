d7b3940384e240dacc3ff6ea9c31a338ac2f8a62
Pablo Felipe <pablofelipe1999@hotmail.com>
2022-04-14 15:05:57 -0300

    Importing Login component and creating loading functionality with spin and Slack logo

--- src/App.js
+++ src/App.js
@@ -12,22 +12,45 @@ import {
 import Header from './components/Header';
 import SideBar from './components/SideBar';
 import Chat from './components/Chat';
+import Login from './components/Login';
 
 // Styled-components:
 import styled from "styled-components"
 
+// React Firebase Hooks:
+import { auth } from './firebase';
+import { useAuthState } from "react-firebase-hooks/auth"
+
+// React Spin-kit:
+import Spinner from "react-spinkit"
+
 function App() {
+  const [user, loading] = useAuthState(auth)
+
+  if (loading) return (
+    <AppLoading>
+      <AppLoadingContent>
+        <img src="https://cdn.mos.cms.futurecdn.net/SDDw7CnuoUGax6x9mTo7dd.jpg" alt="" />
+        <Spinner name="ball-spin-fade-loader" color="purple" fadeIn="none" />
+      </AppLoadingContent>
+    </AppLoading>
+  )
+
   return (
     <div className="App">
       <Router>
-        <Header />
-        <AppBody>
-          <SideBar />
-          <Chat />
-          <Routes>
-            <Route path="/" exact element={<></>}/>
-          </Routes>
-        </AppBody>
+        {!user ? (<Login />) : (
+          <>
+            <AppBody>
+              <Header />
+              <SideBar />
+              <Chat />
+              <Routes>
+                <Route path="/" exact element={<></>}/>
+              </Routes>
+            </AppBody>
+          </>
+        )}
       </Router>
     </div>
   );
@@ -38,4 +61,26 @@ export default App
 const AppBody = styled.div`
   display: flex;
   height: 100vh;
+`
+
+const AppLoading = styled.div`
+  display: grid;
+  place-items: center;
+  height: 100vh;
+  width: 100%;
+`
+
+const AppLoadingContent = styled.div`
+  text-align: center;
+  padding-bottom: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  > img {
+    height: 100px;
+    padding: 20px;
+    margin-bottom: 40px;
+  }
 `
